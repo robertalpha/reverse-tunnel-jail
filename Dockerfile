ef0ead908fdb480c2fed57d006fee873f45fdcab
Robert van Alphen <robert@kramp.com>
2020-05-10 01:30:57 +0200

    introduced tunuser for sole purpose of reverse tunneling limited to a single local tunnel, and not allowed to run commands as a user

--- Dockerfile
+++ Dockerfile
@@ -6,13 +6,23 @@ RUN    apk update \
     && mkdir /root/.ssh \
     && chmod 0700 /root/.ssh \
     && ssh-keygen -A \
-    && sed -i s/^#PasswordAuthentication\ yes/PasswordAuthentication\ no/ /etc/ssh/sshd_config
+    && adduser -D -s /bin/false tunuser \
+    && passwd -u tunuser \
+    && mkdir /home/tunuser/.ssh \
+    && chmod 0700 /home/tunuser/.ssh \
+    && touch /home/tunuser/.ssh/authorized_keys \
+    && chmod 0600 /home/tunuser/.ssh/authorized_keys \
+    && chown -R tunuser:tunuser /home/tunuser \
+    && sed -i s/^#PasswordAuthentication\ yes/PasswordAuthentication\ no/ /etc/ssh/sshd_config \
+    && sed -i s/^#AllowAgentForwarding\ yes/AllowAgentForwarding\ no/ /etc/ssh/sshd_config
 
 # This image expects AUTHORIZED_KEYS environment variable to contain your ssh public key.
 
 COPY docker-entrypoint.sh /
+COPY tunuser-sshd.conf /tmp/
+RUN cat /tmp/tunuser-sshd.conf >> /etc/ssh/sshd_config
 
-EXPOSE 22
+EXPOSE 22 2022
 
 ENTRYPOINT ["/docker-entrypoint.sh"]
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 if [ -z "${AUTHORIZED_KEYS}" ]; then
-  echo "Need your ssh public key as AUTHORIZED_KEYS env variable. Abnormal exit ..."
+  echo "Need an ssh public key as AUTHORIZED_KEYS env variable for remote tunneling for user tunuser to port 2022. Abnormal exit ..."
   exit 1
 fi
 
-echo "Populating /root/.ssh/authorized_keys with the value from AUTHORIZED_KEYS env variable ..."
-echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
+echo "Populating /home/tunuser/.ssh/authorized_keys with the value from AUTHORIZED_KEYS env variable ..."
+echo "${AUTHORIZED_KEYS}" > /home/tunuser/.ssh/authorized_keys
+
+if [ -n "${AUTHORIZED_ROOT_KEYS}" ]; then
+  echo "[WARNING] found AUTHORIZED_ROOT_KEYS env variable and populating /root/.ssh/authorized_keys"
+  echo "[WARNING] please only use AUTHORIZED_ROOT_KEYS when testing and remove before normal use to improve security"
+  echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
+fi
 
 # Execute the CMD from the Dockerfile:
 exec "$@"
