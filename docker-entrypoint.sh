4e1c6575612dae2423a4150a1482eec414d16b12
Robert van Alphen <robert@kramp.com>
2020-05-13 17:57:08 +0200

    included possibility to open multiple ports for reverse tunnelling

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,17 +9,17 @@ echo "${AUTHORIZED_KEYS}" > /home/tunuser/.ssh/authorized_keys
 
 if [ -n "${OPEN_PORTS}" ]; then
   echo "overriding permitted ports to include: ${OPEN_PORTS}"
-  PORT_STRING=$(echo "$OPEN_PORTS" | sed -r 's/,/\ 127\.0\.0\.1:/g')
-  PORT_STRING=$(echo "127.0.0.1:$PORT_STRING")
-  PORT_STRING=$(echo "$PORT_STRING" | sed -r 's/\s/\\ /g')
-  sed -i "s/PermitOpen\ 127\.0\.0\.1:2022/PermitOpen\ $PORT_STRING/g" /etc/ssh/sshd_config
+  PORT_STRING=$(echo "$OPEN_PORTS" | sed -r 's/,/\\ /g')
+  sed -i "s/PermitListen\ 2022/PermitListen\ $PORT_STRING/g" /etc/ssh/sshd_config
 fi
 
 CONTAINER_INTERNAL_IP=$(/sbin/ip route|awk '/src/ { print $7 }')
 
 echo "Running. To test setup run (replace 2022 if you're using OPEN_PORTS):"
-echo " ssh -TN -R 2022:localhost:8022  tunuser@$CONTAINER_INTERNAL_IP"
+echo " ssh -TN -R \*:2022:localhost:8022  tunuser@$CONTAINER_INTERNAL_IP"
 echo "this tunnels you hosts port 8022 to the container's port 2022"
+echo "if you have a webserver running on 8022, then next try:"
+echo " curl $CONTAINER_INTERNAL_IP:2022"
 
 # Execute the CMD from the Dockerfile:
 exec "$@"
